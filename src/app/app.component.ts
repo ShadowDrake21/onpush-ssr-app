c44254fe22abec5553edac3689839fb96441a578
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-01 11:33:10 +0300

    initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'onpush-ssr-dummy-app';
+  title = 'book-angular-app';
 }
