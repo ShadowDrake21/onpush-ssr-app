e48bbe92baef90b33c372b5e576b77adf3f632b2
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-12 20:39:20 +0300

    users: loading and placeholder fixing + empty moving

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,16 +1,22 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  DestroyRef,
+  ElementRef,
+  Host,
+  HostListener,
   inject,
   OnInit,
+  signal,
+  ViewChild,
 } from '@angular/core';
 import { ApiService } from '../../core/services/api.service';
-import { BehaviorSubject, EMPTY, Observable, Subject } from 'rxjs';
+import { EMPTY, map, Observable, of, tap } from 'rxjs';
 import { IUser } from '../../shared/models/user.model';
 import { AsyncPipe, DatePipe, JsonPipe } from '@angular/common';
-import { json } from 'stream/consumers';
-
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-users',
   standalone: true,
@@ -19,20 +25,63 @@ import { json } from 'stream/consumers';
   styleUrl: './users.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit {
   private apiService = inject(ApiService);
   private cdr = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+
+  @ViewChild('movingElement') movingElement!: ElementRef<HTMLDivElement>;
 
   user$!: Observable<IUser>;
 
-  ngOnInit(): void {
-    this.onFetchSingleUser();
+  loadingSig = signal<boolean>(false);
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    if (this.movingElement) {
+      const el = this.movingElement.nativeElement;
+
+      let isDragging = false;
+      let offsetX = 0;
+      let offsetY = 0;
+      // add animation
+      el.addEventListener('mousedown', (event: MouseEvent) => {
+        isDragging = true;
+
+        offsetX = (event.clientX - el.getBoundingClientRect().left) / 2;
+        offsetY = event.clientY - el.getBoundingClientRect().top;
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+      });
+
+      const onMouseMove = (event: MouseEvent) => {
+        if (isDragging) {
+          el.style.top = `${event.clientY - offsetY * 2}px`;
+          el.style.left = `${event.clientX - offsetX - 120}px`;
+        }
+      };
+
+      const onMouseUp = () => {
+        isDragging = false;
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+    }
   }
 
   onFetchSingleUser() {
-    this.user$ = this.apiService.getSingleUser(
-      Math.round(Math.random() * 207 + 1)
-    );
+    this.loadingSig.set(true);
+    this.apiService
+      .getSingleUser(Math.round(Math.random() * 207 + 1))
+      .pipe(
+        map((user) => (this.user$ = of(user))),
+        tap(() => this.loadingSig.set(false)),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
+
     this.cdr.markForCheck();
   }
 
