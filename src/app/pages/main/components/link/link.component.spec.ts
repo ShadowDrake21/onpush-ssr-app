4a694b258d42595a11b5beedbb79d9f8f6812964
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-17 09:53:00 +0300

    testing with jasmine: main page

--- src/app/pages/main/components/link/link.component.spec.ts
+++ src/app/pages/main/components/link/link.component.spec.ts
@@ -1,23 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LinkComponent } from './link.component';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { of } from 'rxjs';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('LinkComponent', () => {
-  let component: LinkComponent;
+fdescribe('LinkComponent', () => {
   let fixture: ComponentFixture<LinkComponent>;
+  let debugEl: DebugElement;
+  let el: HTMLElement;
+  let component: LinkComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LinkComponent]
-    })
-    .compileComponents();
-    
+      imports: [LinkComponent, FontAwesomeModule, RouterLink],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: {
+              paramMap: {
+                get: () => null,
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LinkComponent);
+    debugEl = fixture.debugElement;
+    el = debugEl.nativeElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Presence state', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('Appearance state', () => {
+    it('should have icon by defaut', () => {
+      expect(el.querySelector('fa-icon')).toBeTruthy();
+    });
+  });
+
+  describe('Content state', () => {
+    it('should have a proper href', () => {
+      component.link = '/main';
+      fixture.detectChanges();
+
+      const linkElement: HTMLLinkElement = debugEl.query(
+        By.css('[data-testingId="link"]')
+      ).nativeElement;
+      expect(linkElement.getAttribute('ng-reflect-router-link')).toBe('/main');
+    });
   });
 });
---
+++ src/app/pages/main/main.component.spec.ts
@@ -0,0 +1,110 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MainComponent } from './main.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LinkComponent } from './components/link/link.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+
+fdescribe('MainComponent', () => {
+  let fixture: ComponentFixture<MainComponent>;
+  let debugEl: DebugElement;
+  let el: HTMLElement;
+  let component: MainComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MainComponent, FontAwesomeModule, LinkComponent, RouterModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+            snapshot: {
+              paramMap: {
+                get: () => null,
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
+    fixture = TestBed.createComponent(MainComponent);
+    debugEl = fixture.debugElement;
+    el = debugEl.nativeElement;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  describe('Presence state', () => {
+    it('should title be presented', () => {
+      const { titleElement } = retrieveTitle();
+
+      expect(titleElement).not.toBeNull();
+    });
+
+    it('should imagesContainer be presented', () => {
+      const imagesContainerDebugElement = debugEl.query(
+        By.css('[data-testingId="imagesContainer"]')
+      );
+
+      expect(imagesContainerDebugElement).toBeTruthy();
+    });
+
+    it('should linksContainer be presented', () => {
+      const linksContainerDebugElement = debugEl.query(
+        By.css('[data-testingId="linksContainer"]')
+      );
+
+      expect(linksContainerDebugElement).toBeTruthy();
+    });
+  });
+
+  describe('Content state', () => {
+    it('should title contain a value', () => {
+      const { titleElement } = retrieveTitle();
+
+      expect(titleElement.textContent).toContain(
+        'ChangeDetection.OnPush && SSR Application'
+      );
+    });
+
+    it('should title connect with Github', () => {
+      const { titleElement } = retrieveTitle();
+
+      expect(titleElement.href).toEqual(`https://github.com/ShadowDrake21`);
+    });
+
+    it('should imageContainer contains 2 images', () => {
+      const imagesContainerDebugElement = debugEl.query(
+        By.css('[data-testingId="imagesContainer"]')
+      );
+      const imagesContainerElement: HTMLElement =
+        imagesContainerDebugElement.nativeElement;
+
+      expect(imagesContainerElement.querySelectorAll('img').length).toBe(2);
+    });
+
+    it('should linksContainer have 4 links', () => {
+      const linksContainerDebugElement = debugEl.query(
+        By.css('[data-testingId="linksContainer"]')
+      );
+      const linksContainerElement: HTMLElement =
+        linksContainerDebugElement.nativeElement;
+
+      expect(linksContainerElement.querySelectorAll('main-link').length).toBe(
+        4
+      );
+    });
+  });
+
+  function retrieveTitle() {
+    const titleDebugElement = debugEl.query(
+      By.css('[data-testingId="mainTitle"]')
+    );
+    const titleElement: HTMLLinkElement = titleDebugElement.nativeElement;
+
+    return { titleElement };
+  }
+});
