b57df4f6cbad34d51cd2c09ffcd224f4cdfb31e2
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-07-19 13:44:05 +0300

    testing with jasmine: todos page, part 2, done

--- src/app/pages/todos/todos.component.spec.ts
+++ src/app/pages/todos/todos.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+} from '@angular/core/testing';
 import { TodosComponent } from './todos.component';
 import { ApiService } from '@services/api.service';
 import {
@@ -9,6 +14,7 @@ import { ChangeDetectorRef } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ITodo } from '@shared/models/todo.model';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 fdescribe('TodosComponent', () => {
   let component: TodosComponent;
@@ -45,18 +51,7 @@ fdescribe('TodosComponent', () => {
   });
 
   it('should fetch todos on onTodosFetch', () => {
-    const mockTodos: ITodo[] = [
-      {
-        id: 1,
-        todo: 'Test Todo',
-        completed: false,
-        userId: 4,
-      },
-    ];
-
-    spyOn(apiService, 'getRandomTodo').and.returnValue(of(mockTodos));
-
-    component.onTodosFetch();
+    const { mockTodos } = todoSpyHelper(apiService, component);
 
     expect(apiService.getRandomTodo).toHaveBeenCalledWith(1);
     component.todos$.subscribe((todos) => {
@@ -64,4 +59,83 @@ fdescribe('TodosComponent', () => {
     });
     expect(component.actionTitle$$.getValue()).toBe('Regenerate!');
   });
+
+  it('should show retrieved todos in ui', () => {
+    todoSpyHelper(apiService, component);
+
+    fixture.detectChanges();
+
+    const renderedTodo = fixture.debugElement.query(
+      By.css('[data-testingId="todo"')
+    );
+
+    expect(renderedTodo).toBeTruthy();
+  });
+
+  it('should delete a todo on onClick', fakeAsync(() => {
+    const { mockTodos } = todoSpyHelper(apiService, component);
+
+    component.todos$ = of(mockTodos);
+    fixture.detectChanges();
+    flush();
+
+    let renderedTodo = fixture.debugElement.query(
+      By.css('[data-testingId="todo"')
+    );
+    expect(renderedTodo).toBeTruthy();
+
+    component.onRemoveTodo(mockTodos[0].id);
+    component.todos$.subscribe();
+    uiUpdate<TodosComponent>(fixture);
+
+    renderedTodo = fixture.debugElement.query(By.css('[data-testingId="todo"'));
+
+    expect(renderedTodo).toBeNull();
+  }));
+
+  it('should change a todo on onClick', fakeAsync(() => {
+    const { mockTodos } = todoSpyHelper(apiService, component);
+
+    component.todos$ = of(mockTodos);
+    uiUpdate<TodosComponent>(fixture);
+
+    let renderedTodo = fixture.debugElement.query(
+      By.css('[data-testingId="todo"')
+    );
+    expect(renderedTodo).toBeTruthy();
+
+    component.onRegenerateTodo(mockTodos[0].id);
+
+    component.todos$ = of([
+      { id: 45, todo: 'Regenerated todo', completed: true, userId: 45 },
+    ]);
+    uiUpdate<TodosComponent>(fixture);
+
+    renderedTodo = fixture.debugElement.query(By.css('[data-testingId="todo"'));
+    expect(renderedTodo.nativeElement.textContent).toContain(
+      'Regenerated todo'
+    );
+  }));
 });
+
+function todoSpyHelper(apiService: ApiService, component: TodosComponent) {
+  const mockTodos: ITodo[] = [
+    {
+      id: 1,
+      todo: 'Test Todo',
+      completed: false,
+      userId: 4,
+    },
+  ];
+
+  spyOn(apiService, 'getRandomTodo').and.returnValue(of(mockTodos));
+
+  component.onTodosFetch();
+
+  return { mockTodos };
+}
+
+function uiUpdate<T>(fixture: ComponentFixture<T>) {
+  fixture.detectChanges();
+  flush();
+}
